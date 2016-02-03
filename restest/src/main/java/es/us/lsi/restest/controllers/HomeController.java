8eae13c0eb12e69e4b39b19aa132ddd048f30527
Pepe <peligrossof@gmail.com>
2016-02-03 19:50:12 +0100

    Commit inicia
    Primer commit

---
+++ restest/src/main/java/es/us/lsi/restest/controllers/HomeController.java
@@ -0,0 +1,27 @@
+package es.us.lsi.restest.controllers;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	private static final Logger log = Logger.getLogger(HomeController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		
+		log.info("Welcome page execution in process");
+		
+		return "home";
+	}
+
+}
---
+++ restest/src/main/webapp/WEB-INF/views/header.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+    <title>Home</title>
+    <!-- Compiled and minified CSS -->
+	<link rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+	
+	<!-- Compiled and minified JavaScript -->
+	<script
+		src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
